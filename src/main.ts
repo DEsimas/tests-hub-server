ab7b605ca3ad5046209c874f0b8cb71d4a23792a
DEsimas <simasuh@gmail.com>
2021-12-17 18:10:00 +0300

    root: dependencies management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { config } from 'dotenv';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log(`Running on ${process.env.PORT || 3000}`);
+  });
 }
+
+config();
 bootstrap();
