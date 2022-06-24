20667ced6789be8b597999f1ea483df0642947af
Serafim Sukharev <simasuh@gmail.com>
2022-06-24 17:53:08 +0300

    pinging script for preventing server form idling

--- src/worker.ts
+++ src/worker.ts
@@ -1,8 +1,8 @@
-
 import { Bot as EmojisBot } from 'emojis-bot';
 
 import { Bot as HeysBot } from 'heys-bot';
 
+import fetch from "node-fetch-commonjs";
 
 async function bootstrap() {
     const emojis = new EmojisBot({token: process.env.EMOJIS_BOT_TOKEN, mongo_uri: process.env.EMOJIS_BOT_DB_URI});
@@ -11,5 +11,13 @@ async function bootstrap() {
     const heys = new HeysBot({token: process.env.HEYS_BOT_TOKEN, mongo_uri: process.env.HEYS_BOT_DB_URI});
     heys.start();
   }
+
+async function ping() {
+  const res = await fetch(process.env.SERVER_URL);
+  console.log(await res.text());
+}
   
-  bootstrap();
+bootstrap();
+
+ping();
+setInterval(ping, 1000*60*3) // ping every 3 minutes
