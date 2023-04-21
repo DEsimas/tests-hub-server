c20fab6a74fc0f3992663766a62af063b3992b22
DEsimas <simasuh@gmail.com>
2023-04-21 17:27:56 +0300

    allow cors from anywhere

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: '*',
+        methods: 'GET, PUT, POST, DELETE',
+        allowedHeaders: 'Content-Type, Authorization'
+    });
     app.use((res, req, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
